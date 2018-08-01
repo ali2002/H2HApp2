4ceb2ef93824c5b1e5aabf6af0191740b2072349
Ali Assiri <ali.assiri@blackdoors.co>
2018-08-01 20:12:17 +0300

    Initial commit

---
+++ app/src/main/java/com/hajj/alias/h2happ/LoginAct.java
@@ -0,0 +1,13 @@
+package com.hajj.alias.h2happ;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
